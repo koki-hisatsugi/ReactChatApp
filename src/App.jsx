eecd4540e8d72ca5956371004c307c1c88b8db06
koki-hisatsugi <75100789+koki-hisatsugi@users.noreply.github.com>
2021-06-15 21:13:24 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <input value="チャット" />
+        <button>送信</button>
+      </div>
+    </>
+  );
+}
